6cfdd9f79eb546942efaeae57a2e54e392533825
Alex Kovrigin <a.kovrigin0@gmail.com>
2019-02-05 13:56:36 +0300

    microfixes

--- main_page/home_page_files/html_loader.js
+++ main_page/home_page_files/html_loader.js
@@ -10,14 +10,14 @@ function includeHTML() {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+        if (this.readyState === 4) {
+          if (this.status === 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status === 404) {elmnt.innerHTML = "Page not found.";}
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("include-html");
           includeHTML();
         }
-      }
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /* Exit the function: */
